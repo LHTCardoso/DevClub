e86bc8ab54126e718d26719bd7b20e1b53b4012a
Luiz Cardoso <115110679+LHTCardoso@users.noreply.github.com>
2025-11-03 11:58:41 -0300

    Add files via upload
    revendo conceitos de js

---
+++ Revendo-js/tipoDeDado.js
@@ -0,0 +1,16 @@
+//objeto
+//
+
+const luiz = {
+  name: "Luiz Cardoso",
+  age: 24,
+  address: {
+    street: "Rua Ficticia",
+    number: 10,
+    city: "Cidade Fantasma",
+    state: "Espirito Santo",
+    country: "Brasil",
+  },
+};
+
+console.log(luiz);
